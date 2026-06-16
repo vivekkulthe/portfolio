5ee5a12cd4f7988ef607f7751ee69a30e0f36afa
vivekkulthe <vivekkulthe@users.noreply.github.com>
2026-06-17 00:14:04 +0530

    aaa

--- lib/siteData.ts
+++ lib/siteData.ts
@@ -65,7 +65,7 @@ export const servicesData = [
 
 
 export const experienceData = {
-    yearsOfExperience: '10+',
+    yearsOfExperience: '20+',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
 }
 
