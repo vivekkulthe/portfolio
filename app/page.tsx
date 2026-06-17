6f4e069a91992d813aba21bd536c2d55555b5304
vivekkulthe <vivekkulthe@users.noreply.github.com>
2026-06-17 23:09:45 +0530

    removed skills bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
 import Resume from "@/components/Resume";
 import Services from "@/components/Services";
-import Skills from "@/components/Skills";
+/*import Skills from "@/components/Skills";*/
 import SlidingText from "@/components/SlidingText";
 
 export default function Home() {
@@ -25,8 +25,6 @@ export default function Home() {
             <Portfolio />
 
             <div className="container md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1320px] mx-auto px-7">
-                {/* Skills */}
-                <Skills />
 
                 {/* Resume */}
                 <Resume />
